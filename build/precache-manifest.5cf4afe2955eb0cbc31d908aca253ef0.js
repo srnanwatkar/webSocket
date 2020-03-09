ba5d524635b04d6efe8f6ea371e84b4a24dad616
Shantanu Nanwatkar <srnanwatkar@gmail.com>
2020-03-09 14:44:15 +0530

    updated build

---
+++ build/precache-manifest.5cf4afe2955eb0cbc31d908aca253ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cc6ccf3f49d21bc1c0340179ba0916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2b1e1fe6efe7b2ebc5",
+    "url": "/static/css/main.4ebb898a.chunk.css"
+  },
+  {
+    "revision": "fa7df35b3d8a90286db5",
+    "url": "/static/js/2.f7282b8e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f7282b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2b1e1fe6efe7b2ebc5",
+    "url": "/static/js/main.504a3895.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
